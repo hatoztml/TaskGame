5fc42bf155e76d3facd81d2c29ae450cd44b8a76
Hatip Oztemel <hatoztemel@gmail.com>
2019-09-25 18:08:25 +0300

    added printArray method

--- game.py
+++ game.py
@@ -18,9 +18,21 @@ class Array:
                 gen += 1
             self.myArray.append(temp_array)
             yuk += 1
+
+    def printArray(self):
+        print("Yukseklik =", self.yuk, "Ve  Genislik =", self.gen)
+        for i in range(self.yuk):
+            for j in range(self.gen):
+                if self.myArray[i][j]:
+                    print("1|", end='')
+                else:
+                    print(" |", end='')
+            print()
+        print("--------------------------------------------")
     
 if __name__ == '__main__':
     gen = random.randrange(25, 30)
     yuk = random.randrange(10, 15)
     hello = Array(yuk,gen)
     hello.randomArray()
+    hello.printArray()
