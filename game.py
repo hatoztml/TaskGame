2b581ab49136faf597c163a0508e97b7ff9f2820
Hatip Oztemel <hatoztemel@gmail.com>
2019-09-25 17:59:36 +0300

    Created a random array for project

--- game.py
+++ game.py
@@ -7,8 +7,20 @@ class Array:
         self.gen = gen
         self.myArray = []
     
+    def randomArray(self):
+        gen = 0
+        yuk = 0
+        while yuk < self.yuk:
+            gen = 0
+            temp_array = []
+            while gen < self.gen:
+                temp_array.append(random.randrange(0,2))
+                gen += 1
+            self.myArray.append(temp_array)
+            yuk += 1
     
 if __name__ == '__main__':
     gen = random.randrange(25, 30)
     yuk = random.randrange(10, 15)
     hello = Array(yuk,gen)
+    hello.randomArray()
