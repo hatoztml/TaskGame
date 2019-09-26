e80a6c2fc92f956b21946f6e167b784bd4ae333f
Hatip Oztemel <hatoztemel@gmail.com>
2019-09-26 10:51:28 +0300

    Added countKomsu method

--- game.py
+++ game.py
@@ -2,22 +2,25 @@ import random
 
 class Array:
 
-    def __init__(self,yuk,gen):
-        self.yuk = yuk
-        self.gen = gen
-        self.myArray = []
-    
-    def randomArray(self):
-        gen = 0
-        yuk = 0
-        while yuk < self.yuk:
-            gen = 0
-            temp_array = []
-            while gen < self.gen:
-                temp_array.append(random.randrange(0,2))
-                gen += 1
-            self.myArray.append(temp_array)
-            yuk += 1
+    def __init__(self, _array, _extraArray=None):
+        self.myArray = _array
+        self.yuk = self.myArray.__len__()
+        self.gen = self.myArray[0].__len__()
+        self.extraArray = _extraArray
+
+
+    @staticmethod
+    def randomArray():
+        width = random.randrange(25, 30)
+        height = random.randrange(10, 15)
+        array = [[0] * width for i in range(height)]
+        for i in range(height):
+            for j in range(width):
+                if random.randrange(0, 2) == 1:
+                    array[i][j] = 1
+                else:
+                    array[i][j] = 0
+        return Array(array)
 
     def printArray(self):
         print("Yukseklik =", self.yuk, "Ve  Genislik =", self.gen)
@@ -28,11 +31,72 @@ class Array:
                 else:
                     print(" |", end='')
             print()
-        print("--------------------------------------------")
+        print("\n--------------------------------------------")
+    pass
+
+    def countKomsu(self,eksenX,eksenY):
+
+        toplam = 0        
+        if eksenX == 0 and eksenY == 0:
+            toplam += self.myArray[eksenY + 1][eksenX]
+            toplam += self.myArray[eksenY + 1][eksenX + 1]
+            toplam += self.myArray[eksenY][eksenX + 1]
+        
+        elif eksenX == self.gen-1 and eksenY == self.yuk-1:
+            toplam += self.myArray[eksenY - 1][eksenX]
+            toplam += self.myArray[eksenY - 1][eksenX - 1]
+            toplam += self.myArray[eksenY][eksenX - 1]
+        
+        elif eksenX == 0 and eksenY == self.yuk - 1:
+            toplam += self.myArray[eksenY][eksenX + 1]
+            toplam += self.myArray[eksenY - 1][eksenX + 1]
+            toplam += self.myArray[eksenY - 1][eksenX]
+        
+        elif eksenX == self.gen-1 and eksenY == 0:
+            toplam += self.myArray[eksenY][eksenX - 1]
+            toplam += self.myArray[eksenY + 1][eksenX - 1]
+            toplam += self.myArray[eksenY + 1][eksenX]
+        
+        elif eksenY == 0 and eksenX != 0 and eksenX != self.gen - 1:
+            toplam += self.myArray[eksenY + 1][eksenX]
+            toplam += self.myArray[eksenY + 1][eksenX + 1]
+            toplam += self.myArray[eksenY + 1][eksenX - 1]
+            toplam += self.myArray[eksenY][eksenX - 1]
+            toplam += self.myArray[eksenY][eksenX + 1]
+        
+        elif eksenY == self.yuk - 1 and eksenX != 0 and eksenX != self.gen - 1:
+            toplam += self.myArray[eksenY - 1][eksenX]
+            toplam += self.myArray[eksenY - 1][eksenX + 1]
+            toplam += self.myArray[eksenY - 1][eksenX - 1]
+            toplam += self.myArray[eksenY][eksenX + 1]
+            toplam += self.myArray[eksenY][eksenX - 1]
+        
+        elif eksenX == 0 and eksenY != 0 and eksenY != self.yuk - 1:
+            toplam += self.myArray[eksenY][eksenX + 1]
+            toplam += self.myArray[eksenY + 1][eksenX + 1]
+            toplam += self.myArray[eksenY - 1][eksenX + 1]
+            toplam += self.myArray[eksenY + 1][eksenX]
+            toplam += self.myArray[eksenY - 1][eksenX]
+        
+        elif eksenX == self.gen-1 and eksenY != 0 and eksenY != self.yuk - 1:
+            toplam += self.myArray[eksenY][eksenX -1]
+            toplam += self.myArray[eksenY + 1][eksenX - 1]
+            toplam += self.myArray[eksenY - 1][eksenX - 1]
+            toplam += self.myArray[eksenY + 1][eksenX]
+            toplam += self.myArray[eksenY - 1][eksenX]
+        
+        else:
+            toplam += self.myArray[eksenY][eksenX - 1]
+            toplam += self.myArray[eksenY][eksenX + 1]
+            toplam += self.myArray[eksenY + 1][eksenX + 1]
+            toplam += self.myArray[eksenY + 1][eksenX - 1]
+            toplam += self.myArray[eksenY - 1][eksenX + 1]
+            toplam += self.myArray[eksenY - 1][eksenX - 1]
+            toplam += self.myArray[eksenY + 1][eksenX]
+            toplam += self.myArray[eksenY - 1][eksenX]
+        return toplam
     
+
 if __name__ == '__main__':
-    gen = random.randrange(25, 30)
-    yuk = random.randrange(10, 15)
-    hello = Array(yuk,gen)
-    hello.randomArray()
+    hello = Array.randomArray()
     hello.printArray()
